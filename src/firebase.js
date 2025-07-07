fa8325b141f6f214423a0c610c0ef0a26c3f065c
6127613876-1 <gokulpriyan781@gmail.com>
2025-07-07 18:55:13 +0530

    3

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,24 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref } from 'firebase/database'
+import { getDatabase, ref } from 'firebase/database';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDXg3AKBNRYTnmwt3F6aflYGXPU4a_Thyg",
-  authDomain: "cloud-ba3f6.firebaseapp.com",
-  databaseURL: "https://cloud-ba3f6-default-rtdb.firebaseio.com",
-  projectId: "cloud-ba3f6",
-  storageBucket: "cloud-ba3f6.appspot.com",
-  messagingSenderId: "436721807147",
-  appId: "1:436721807147:web:d71c1408813d3bae1c9f6e",
-  measurementId: "G-81SD9K2X1H"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app)
-export const dbRef = (path) => ref(db, path)
-const auth = getAuth(app)
-const googleProvider  = new GoogleAuthProvider();
+export const db = getDatabase(app);
+export const dbRef = (path) => ref(db, path);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-export { auth, googleProvider , signInWithPopup, signOut };
+export { auth, googleProvider, signInWithPopup, signOut };
